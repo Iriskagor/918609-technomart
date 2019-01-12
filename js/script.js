b6ad1333271c215855685c91b3d39bc11c1505d2
Iriskagor <44980743+Iriskagor@users.noreply.github.com>
2019-01-12 14:11:28 +0300

    finish index and catalog pages

--- js/script.js
+++ js/script.js
@@ -13,21 +13,56 @@ document.addEventListener("DOMContentLoaded", function(){
       document.getElementById(id).className = "services__items__description active";
     });
   });
+  var openMap = document.querySelector('.information__map');
+  if (openMap){
+    openMap.addEventListener('click', function(){
+      document.querySelector('.information__map-popup').className = "information__map-popup";
+    })
+  }
+  var closeMap = document.querySelector('.information__map-popup__btn-close');
+  if (closeMap){
+    closeMap.addEventListener('click', function(){
+      document.querySelector('.information__map-popup').className = "information__map-popup visually-hidden";
+    })
+  }
 
-  document.querySelector('.information__map').addEventListener('click', function(){
-    document.querySelector('.information__map-popup').className = "information__map-popup";
-  })
+  var openInfo = document.querySelector('.open-information-popup');
+  if (openInfo){
+    openInfo.addEventListener('click', function(e){
+      e.preventDefault();
+      document.querySelector('.information__writeus').className = "information__writeus";
+    })
+  }
 
-  document.querySelector('.information__map-popup__btn-close').addEventListener('click', function(){
-    document.querySelector('.information__map-popup').className = "information__map-popup visually-hidden";
-  })
+  var closeInfo = document.querySelector('.information__writeus__btn-close')
+  if (closeInfo){
+    closeInfo.addEventListener('click', function(){
+      document.querySelector('.information__writeus').className = "information__writeus visually-hidden";
+    })
+  }
 
-  document.querySelector('.open-information-popup').addEventListener('click', function(e){
-    e.preventDefault();
-    document.querySelector('.information__writeus').className = "information__writeus";
-  })
+  var addToCartButtons = Array.from(document.getElementsByClassName('product-list__card__buy'));
+  console.log(addToCartButtons.length);
+  if (addToCartButtons.length > 0){
+    addToCartButtons.forEach(function(el){
+      el.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector('.addtocard__popup').classList.remove('visually-hidden');
+      })
+    })
+  }
 
-  document.querySelector('.information__writeus__btn-close').addEventListener('click', function(){
-    document.querySelector('.information__writeus').className = "information__writeus visually-hidden";
-  })
+  var addToCartClose = document.querySelector('.addtocard__popup__btn-close');
+  if (addToCartClose){
+    addToCartClose.addEventListener('click', function(){
+      document.querySelector('.addtocard__popup').classList.add('visually-hidden');
+    })
+  }
+
+  var continueShopping = document.querySelector('.addtocard__buttons__wrapper button');
+  if (continueShopping){
+    continueShopping.addEventListener('click', function(){
+      document.querySelector('.addtocard__popup').classList.add('visually-hidden');
+    })
+  }
 });
